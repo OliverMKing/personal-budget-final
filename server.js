3b03e91c1c4c04c0e3c2b376c6381601a01f203d
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-11-02 17:28:41 -0500

    Add router and server

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const exjwt = require("express-jwt");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
+  next();
+});
+
+const PORT = 3001;
+
+const secretKey = "This should be changed to an env variable";
+const jwtMW = exjwt({
+  secret: secretKey,
+  algorithms: ["HS256"],
+});
+
+// Logs in user
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  for (let user of [{ username: "test", password: "test" }]) {
+    if (username === user.username && password === user.password) {
+      let token = jwt.sign(
+        { id: user.id, username: user.username },
+        secretKey,
+        { expiresIn: "7d" }
+      );
+      res.json({
+        success: true,
+        err: null,
+        token,
+      });
+      break;
+    } else {
+      res.status(401).json({
+        success: false,
+        token: null,
+        err: "Username or password is incorrect",
+      });
+    }
+  }
+});
+
+app.get("/api/isLoggedIn", jwtMW, (req, res) => {
+  res.json({
+    success: true,
+    loggedIn: true,
+  });
+});
+
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({
+      success: false,
+      officialError: err,
+      err: "You must be logged in to do that",
+    });
+  } else {
+    next(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Serving on port ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Index from "./Pages/Index";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <Index />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
